06e57099882e4c7e266e8cf6fa366a1c2dafc57a
TudorAndrei <tudorandrei.dumitrascu@gmail.com>
2021-11-02 14:37:21 +0200

    update

--- trn.sh
+++ trn.sh
@@ -4,14 +4,13 @@ python train.py \
     --batch 32 \
     --weights yolov5l.pt \
     --data data.yaml \
-    --epochs 200 \
+    --epochs 300 \
     --img 320 \
     --device 0 \
     --cache \
     --freeze 10\
-    --adam \
     --hyp  data/hyps/hyp.finetune.yaml \
+    --adam \
     # --patience 5 \
-    # --freeze 10\
     # --hyp  data/hyps/hyp.custom_train.yaml \
     # --name "TL custom_train"
